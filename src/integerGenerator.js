886dd3e33c079ebabbb90e3663126dddc6189785
Omar Abdelaziz Oueslati <ooueslati@cimpress.com>
2017-03-30 20:41:41 +0100

    Add the publisher of random integers

---
+++ src/integerGenerator.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var randomInt = require('random-int');
+var connection = require('amqplib').connect('amqp://localhost');
+
+var publish = function(){
+	connection.then(function(conn) {
+		return conn.createChannel().then(function(channel) {
+			var queue = 'randomIntegersQueue';
+			var ok = channel.assertQueue(queue, {durable: true})
+
+			var message = randomInt(1, 10000).toString();
+
+			return ok.then(function() {
+				channel.sendToQueue(queue, new Buffer(message));
+				console.log(" Message published '%s'", message);
+				return channel.close();
+			});
+		}).catch(function(err) { conn.close(); });
+	});
+}
+
+setInterval(publish, 100);
